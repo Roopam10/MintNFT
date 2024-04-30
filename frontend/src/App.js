f2a67d023acaae9faf186f9f31348209a520568c
Roopam10 <roopampatil10@gmail.com>
2024-04-30 23:57:39 +0530

    added pages for login, signup ,home,profile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Nav from './components/Nav';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Footer from './components/Footer';
+import SignUp from './components/SignUp';
+import PrivateComponent from './components/PrivateComponent';
+import Login from './components/Login';
+import Home from './components/Home';
+import Profile from './components/Profile';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+        <Nav />
+        <Routes>
+
+          <Route element={<PrivateComponent/>}>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/logout' element={<h1>LogOut</h1>}></Route>
+          <Route path='/profile' element={<Profile/>}></Route>
+          </Route>
+          
+          <Route path='/signup' element={<SignUp/>}></Route>
+          <Route path='/login' element={<Login/>}></Route>
+        </Routes>
+        </BrowserRouter>
+        <Footer/>
+      </div>
+    );
+  }
 }
 
 export default App;
